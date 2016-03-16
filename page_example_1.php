55dab44a57ef14af9565a316c7bddab6a3c6a519
Kulllord <kulllord@gmail.com>
2016-03-15 22:12:21 -0500

    removing former test content

--- page_example_1.php
+++ page_example_1.php
@@ -2,7 +2,7 @@
 /**
  * Template Name: Testing Template 1
  *
- * This is the template that displays all pages by default.
+ * This is an example template.
  * Please note that this is the WordPress construct of pages
  * and that other 'pages' on your WordPress site may use a
  * different template.
@@ -14,48 +14,25 @@
 
 get_header(); ?>
 
-<?php
-
-$textContent = array();
-
-$textContent['Starts_With_p_Tag'] = '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
-$textContent['Ends_With_p_Tag'] = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>';
-$textContent['Has_p_Tags'] = '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>';
-
-
-
-function pure_text( $content ) {
-	return htmlspecialchars($content);
-}
-
-?>
-
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-		<div style="margin: 0 25px;">
-			<?php foreach ( $textContent as $title => $text ) :
-				$returned = cmmp_remove_wpautop_debug( $text, true ); ?>
-			<div>
-				<h3><?php echo $title; ?></h3>
-				<h4>original</h4>
-				<div>
-					<code><?php echo pure_text( $text ); ?></code>
-				</div>
-				<h4>parsed</h4>
-				<?php
-				foreach ( $returned as $title_2 => $output ) : ?>
-					<h5><?php echo $title_2; ?></h5>
-					<div>
-						<code><?php echo pure_text( $output ); ?></code>
-					</div>
-				<?php endforeach; ?>
-			</div>
-			<?php endforeach; ?>
-		</div>
+			<?php
+			while ( have_posts() ) : the_post();
+
+				get_template_part( 'template-parts/content', 'page' );
+
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
+
+			endwhile; // End of the loop.
+			?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
 <?php
+get_sidebar();
 get_footer();
