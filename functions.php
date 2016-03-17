1ab5fa67547395afac436ca9989569323d40cc6b
Kulllord <kulllord@gmail.com>
2016-03-17 10:30:28 -0500

    removing unneeded code

--- functions.php
+++ functions.php
@@ -111,22 +111,6 @@ function cmm_s_widgets_init() {
 add_action( 'widgets_init', 'cmm_s_widgets_init' );
 
 /**
- * Enqueue scripts and styles.
- */
-/*function cmm_s_scripts() {
-	wp_enqueue_style( 'cmm_s-style', get_stylesheet_uri() );
-
-	wp_enqueue_script( 'cmm_s-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
-
-	wp_enqueue_script( 'cmm_s-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
-
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
-}
-add_action( 'wp_enqueue_scripts', 'cmm_s_scripts' );*/
-
-/**
  * Implement the Custom Header feature.
  */
 require get_template_directory() . '/inc/custom-header.php';
